7f8ef6d6f041748c1a5852979a2f755c5b902026
gibranlp <gibranlp@gmail.com>
2021-01-04 20:00:51 -0500

    Merge

---
+++ dotfiles/.config/qtile/config.py
@@ -0,0 +1,19 @@
+# _______  _______  ______  _______  __        
+#|       ||   _   ||   __ \|     __||  |.-----.
+#|   -  _||       ||      <|__     ||  ||  _  |
+#|_______||___|___||___|__||_______||__||   __|
+#                                       |__|   
+# QARSlp Qtile + Arch Ricing Script
+# By: gibranlp <thisdoesnotwork@gibranlp.dev>
+# MIT licence 
+#
+ 
+import os, re
+from libqtile.config import Key,Group, Match, Drag, Click, Rule
+from libqtile.command import lazy
+from hooks import *
+from theme import *
+from keys import *
+
+#wmname = "LG3D"
+wmname = "qtile"
