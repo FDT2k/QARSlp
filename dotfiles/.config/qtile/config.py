bb69ceb40d23507eff9f494b76d4553b5ca55615
gibranlp <root@gibranlp.dev>
2021-02-19 19:26:07 -0500

    Qtile 7 fixes / changes

---
+++ dotfiles/.config/qtile/config.py
@@ -0,0 +1,15 @@
+# _______  _______  ______  _______  __        
+#|       ||   _   ||   __ \|     __||  |.-----.
+#|   -  _||       ||      <|__     ||  ||  _  |
+#|_______||___|___||___|__||_______||__||   __|
+#                                       |__|   
+# QARSlp Qtile + Arch Ricing Script
+# By: gibranlp <thisdoesnotwork@gibranlp.dev>
+# MIT licence 
+#
+ 
+from hooks import *
+from theme import *
+from keys import *
+#wmname = "LG3D"
+wmname = "qtile"
