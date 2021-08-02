98a79a81e893e72ef0e00f162cc8ad0aab8a92bd
gibranlp <gibranlp@gmail.com>
2021-08-01 21:42:26 -0500

    updates

--- dotfiles/.config/qtile/theme.py
+++ dotfiles/.config/qtile/theme.py
@@ -1,286 +1,10 @@
-# _______  _______  ______  _______  __        
-#|       ||   _   ||   __ \|     __||  |.-----.
-#|   -  _||       ||      <|__     ||  ||  _  |
-#|_______||___|___||___|__||_______||__||   __|
-#                                       |__|   
-# QARSlp Qtile + Arch Ricing Script
-# By: gibranlp <thisdoesnotwork@gibranlp.dev>
-# MIT licence 
-#
-import os
-import re
-import socket
-import subprocess
-import json
-from libqtile import qtile
-from libqtile import hook
-from libqtile.command import lazy
-from libqtile.config import Screen, Key, Group, Match, Drag, Click, Rule
-from libqtile import qtile, layout, bar, widget
-from libqtile.lazy import lazy
-from subprocess import run
-
-#### Variables ####
-mod = "mod4"
-alt = "mod1"                                   
-term = "alacritty"
-home = os.path.expanduser('~')
-prompt = "{0}@{1}: ".format(os.environ["USER"], socket.gethostname())
-#### End variables ####
-
-##### Import Pywal Palette #####
-with open(home + '/.cache/wal/colors.json') as json_file:
-    data = json.load(json_file)
-    colorsarray = data['colors']
-    val_list = list(colorsarray.values())
-    def getList(val_list):
-        return [*val_list]
-
-def init_colors():
-    return [*val_list]
-
-colors = init_colors()
-##### End Import Pywal Palette #####
-
-
-
-#### Import Network Interface ####
-with open(home + '/.config/qtile/actnet', 'r') as file:
-    netact = file.read().replace('\n', '')
-#### End Import Network Interface ####
-
-
-#### Hooks ####
-@hook.subscribe.startup_once
-def start_once():
-    subprocess.call('/opt/bin/autostart')
-
-@hook.subscribe.startup
-def start():
-    subprocess.call('/opt/bin/alwaystart')
-    subprocess.call('/opt/bin/wvis')
-
-@hook.subscribe.client_new
-def floating(window):
-    floating_types = ['notification', 'toolbar', 'splash', 'dialog','Nextcloud','Gcr-prompter','lxappearance','Pavucontrol','pavucontrol','_NET_WM_WINDOW_TYPE_NORMAL']
-    transient = window.window.get_wm_transient_for()
-    if window.window.get_wm_type() in floating_types or transient:
-        window.floating = True
-#### End hooks ####
-
-##### Functions #####
-@lazy.function
-def window_to_prev_group(qtile):
-    if qtile.currentWindow is not None:
-        i = qtile.groups.index(qtile.currentGroup)
-        qtile.currentWindow.togroup(qtile.groups[i - 1].name)
-
-@lazy.function
-def window_to_next_group(qtile):
-    if qtile.currentWindow is not None:
-        i = qtile.groups.index(qtile.currentGroup)
-        qtile.currentWindow.togroup(qtile.groups[i + 1].name)
-##### Specific Apps/Groups / Apps/Grupos especificos #####
-
-def app_or_group(group, app):
-    def f(qtile):
-        if qtile.groups_map[group].windows:
-            qtile.groups_map[group].cmd_toscreen(toggle=False)
-            qtile.cmd_spawn(app)
-        else:
-            qtile.groups_map[group].cmd_toscreen(toggle=False)
-            qtile.cmd_spawn(app)
-    return f
-
-#### Functions ####
-def ncsp(qtile):
-    qtile.groups_map["7"].cmd_toscreen(toggle=False)
-    qtile.cmd_spawn(term + ' -e bash -c ". ~/.zshrc; ncspot"')
-
-def ranger(qtile):
-    qtile.groups_map["1"].cmd_toscreen(toggle=False)
-    qtile.cmd_spawn(term + ' -e bash -c ". ~/.zshrc; ranger"')
-
-def cranger(qtile):
-    qtile.groups_map["1"].cmd_toscreen(toggle=False)
-    qtile.cmd_spawn(term + ' -e bash -c ". ~/.zshrc; ranger"')
-
-def wsearx():
-    qtile.groups_map["4"].cmd_toscreen(toggle=False)
-    run('/opt/bin/wsearch')
-
-def cfilex():
-    qtile.groups_map["1"].cmd_toscreen(toggle=False)
-    qtile.cmd_spawn('nautilus')
-
-def ksearx(qtile):
-    qtile.groups_map["4"].cmd_toscreen(toggle=False)
-    run('/opt/bin/wsearch')
-
-def wnetw():
-    qtile.cmd_spawn('/opt/bin/network')
-
-
-def wsess():
-    run('/opt/bin/logout')
-#### End Functions ####
-
-#### Layouts ####
-def init_layout_theme():
-    return {"font":"Fira Code Medium",
-            "fontsize":16,
-            "margin": 10,
-            "border_width":3,
-            "border_normal":colors[0],
-            "border_focus":colors[5],
-            "single_margin":0,
-            "single_border_width":0,
-           }
-
-def init_layouts():
-    return [#layout.MonadWide(**layout_theme),
-            #layout.Bsp(**layout_theme),
-            #layout.Stack(stacks=2, **layout_theme),
-            #layout.Columns(**layout_theme),
-            #layout.RatioTile(**layout_theme),
-            #layout.VerticalTile(**layout_theme),
-            #layout.Tile(shift_windows=True, **layout_theme),
-            layout.Matrix(**layout_theme),
-            #layout.Zoomy(**layout_theme),
-            layout.MonadTall(max_ratio=0.80, ratio=0.70, **layout_theme),
-            #layout.Max(**layout_theme),
-            layout.TreeTab(sections=["Tabs"],section_fontsize=15, bg_color=colors[0], active_bg=colors[7], active_fg=colors[0], inactive_bg=colors[0], inactive_fg=colors[7],padding_y=5,panel_width=250, **layout_theme),
-            layout.Floating(**layout_theme)]
-#### End layouts ####
-
-##### Groups #####
-groups = [
-    Group("1",position=1,matches=[Match(wm_class=['nautilus', 'gnome-disks', 'Gnome-disks', 'anydesk', 'Simplenote', 'Anydesk'])],layout="monadtall",label=""),
-    Group("2",position=2,matches=[Match(wm_class=['Zoom','zoom', 'Mailspring', 'mailspring', 'transmission-gtk','Transmission-gtk'])],layout="monadtall",label=""),
-    Group("3",position=3,matches=[Match(wm_class=['whatsdesk','telegram-desktop-bin', 'TelegramDesktop', 'Discord', 'discord'])],layout="matrix",label=""),
-    Group("4",position=4,matches=[Match(wm_class=['firefox'])],layout="monadtall",label=""),
-    Group("5",position=5,matches=[Match(wm_class=['Code', 'code','Filezilla'])],layout="monadtall",label=""),
-    Group("6",position=6,matches=[Match(wm_class=['Gimp-2.10','Inkscape','Evince', 'libreoffice','Com.github.phase1geo.minder'])],layout="monadtall",label=""),
-    Group("7",position=7,layout="monadtall",label=""),
-    Group("8",position=8,matches=[Match(wm_class=['VirtualBox Manager', 'VirtualBox Machine'])],layout="monadtall",label=""),
-    Group("9",position=9,layout="monadtall",label="")]
-#### End Groups ####
-
-#### Keys ####
-def init_keys():
-    keys = [ 
-            #### Basics ####          
-            Key([mod], "Return", lazy.spawn(term)), # Open Terminal
-            Key([mod, "shift"], "Return", lazy.spawn('rofi -theme "~/.config/rofi/launcher.rasi" -show drun')),
-            Key([mod, "mod1"], "Return", lazy.spawn('sudo rofi -theme "~/.config/rofi/launcher.rasi" -show drun')), 
-            Key([mod], "q",lazy.window.kill()), # Close Window 
-            Key([mod, "shift"], "r",lazy.restart()), # Restart Qtile
-            Key([mod, "shift"], "q",lazy.shutdown()), # Logout 
-            Key([mod], "Escape", lazy.spawn('xkill')), # Click window to close
-            
-            #### Widgets ####
-            Key([mod],"h",lazy.spawn('/opt/bin/shortc')), # Sortcurts widget
-            Key([mod],"p",lazy.spawn('/opt/bin/qback')), # Launcher
-            Key([mod],"f",lazy.function(ksearx)), # WEB Search
-            Key([mod],"x",lazy.spawn('/opt/bin/logout')),
-            Key([mod],"n",lazy.spawn('/opt/bin/network')),
-            Key([mod, "shift"],"w",lazy.spawn('/opt/bin/qback')),
-            Key([mod, "shift"],"c",lazy.spawn('/opt/bin/fans')),
-
-            #### Add Screen ####
-            Key([mod, "shift"],"y",lazy.spawn(term + ' -e xrandr --output HDMI1 --auto --right-of eDP1')),
-            #### Theming ####
-            Key([mod], "w",lazy.spawn('/opt/bin/genwal')), # Set randwom wallpaper / colors to entire system
-
-            #### Apps ####
-            Key([mod],"e",lazy.function(app_or_group("1", "nautilus"))), #File manager
-            Key([mod, "shift"],"e",lazy.function(ranger)), # CLI file manager
-            Key([mod, "shift"],"a",lazy.function(app_or_group("1", "anydesk"))),
-            Key([mod, "shift"],"s",lazy.function(app_or_group('1', 'simplenote'))),
-
-            ## Group 2 (Organization: Mail)
-            Key([mod],"m",lazy.function(app_or_group('2', 'mailspring'))),
-            
-            ## Group 3 (Social: Whatsapp, Telegram, )
-            Key([mod, "shift"],"t",lazy.function(app_or_group('3', 'telegram-desktop'))),
-            Key([mod, "shift"],"d",lazy.function(app_or_group('3', 'discord'))),
-
-            ## Group 4 (WEB: Firefox)(Admin: Mail, notes, social)
-            Key([mod, "shift"],"f",lazy.function(app_or_group('4', 'firefox'))),
-            
-            ## Group 5 (Code/Write/Office: visual studio, typora, onlyofice)
-            Key([mod],"v",lazy.function(app_or_group('5', 'code'))),
-            Key([mod],"o",lazy.function(app_or_group("6", 'libreoffice'))),
-            Key([mod],"c",lazy.function(app_or_group('5', 'code'))),
-
-            ## Group 6 (Design: Gimp, Inkscape, feh)
-            Key([mod],"g",lazy.function(app_or_group('6', 'gimp'))),
-
-            ## Group 7 (Music: Ncsp, Spotify)
-            Key([mod],"s",lazy.function(ncsp)),
-
-            ## Group 8 (Virtual Stuff games)
-            Key([mod, "shift"],"v",lazy.spawn(term + ' -e bash -c ". ~/.zshrc; vis"')),
-            Key([mod],"b",lazy.function(app_or_group('8', '/home/gibranlp/albiononline/./Albion-Online'))),
-            
-            #### Layouts ####
-            Key([mod], "Tab",lazy.layout.down()), # Change focus of windows down
-            Key([mod, "shift"], "Tab",lazy.layout.up()), # Change focus of windows up
-            Key([alt], "Tab", lazy.layout.swap_left()),
-            Key([alt, "shift"], "Tab", lazy.layout.swap_right()),
-            #Key([mod], "h", lazy.layout.left()),
-            #Key([mod], "l", lazy.layout.right()),
-            #Key([mod], "Up", lazy.layout.down()),
-            #Key([mod], "Down", lazy.layout.up()),
-
-            #### Brightness ####
-            Key([], "XF86MonBrightnessUp", lazy.spawn("xbacklight -inc 5")),
-            Key([], "XF86MonBrightnessDown", lazy.spawn("xbacklight -dec 5")),
-
-            #### Volume ####
-            Key([], "XF86AudioMute", lazy.spawn("amixer -q set Master toggle")),
-            Key([], "XF86AudioLowerVolume", lazy.spawn("amixer -q set Master 5%-")),
-            Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer -q set Master 5%+")),
-
-            #### Media Control ####
-            #Key([mod], "v", lazy.spawn('/home/gibranlp/MEGA/computerStuff/keyboard/keyboard_activate.sh')),
-            #Key([mod], "b", lazy.spawn('/home/gibranlp/MEGA/computerStuff/keyboard/keyboard_deactivate.sh')),
-            Key([], "XF86AudioPlay", lazy.spawn("playerctl -p ncspot play-pause")),
-            Key([], "XF86AudioNext", lazy.spawn("playerctl -p ncspot next")),
-            Key([], "XF86AudioPrev", lazy.spawn("playerctl -p ncspot previous")),
-            Key([], "XF86AudioStop", lazy.spawn("playerctl -p ncspot stop")),
-
-            #### Window hotkeys ####
-            Key([alt], "f", lazy.window.toggle_fullscreen()),
-            Key([alt, "shift"], "f", lazy.window.toggle_floating()),
-            Key([mod], "space", lazy.next_layout()),
-
-            #### Resize windows ####
-            Key([mod, "shift"], "Up", lazy.layout.grow()),
-            Key([mod, "shift"], "Down", lazy.layout.shrink()),
-            Key([mod, "shift"], "space", lazy.layout.flip()),
-
-            ##### Change focus ####
-            Key([mod], "Up", lazy.layout.up()),
-            Key([mod], "Down", lazy.layout.down()),
-            Key([mod], "Left", lazy.layout.left()),
-            Key([mod], "Right", lazy.layout.right()),
-
-            ### Screenshots ####
-            Key([], "Print", lazy.spawn('/opt/bin/screenshot')),]
-
-    for i in groups:
-            keys.append(Key([mod], i.name, lazy.group[i.name].toscreen()))
-            keys.append(Key([mod, 'shift'], i.name, lazy.window.togroup(i.name)))
-    return keys
-#### End Keys ####
-
 #### Widgets ####
+
 def init_widgets_defaults():
     return dict(font="Fira Code Medium",fontsize=16,padding=2,background=colors[0])
 
-def init_widgets_top():    
-    widgets_top = [
+def init_wid_list_top():    
+    wid_list_top = [
                 #### Shortcuts ####
                 widget.TextBox(
                     font='Font Awesome 5 Free',
@@ -385,7 +109,7 @@ def init_widgets_top():
                     padding=5,
                     format='{state}{name}',
                     empty_group_string='QARSlp',
-                    max_chars=70
+                    max_chars=35
                     ),
                 #### Spacer ####
                 #widget.Spacer(
@@ -562,10 +286,10 @@ def init_widgets_top():
                     fontshadow=colors[3]
                     ),
     ]
-    return widgets_top
+    return wid_list_top
 
-def init_widgets_bott():
-    widgets_bott = [
+def in_wid_list_bot():
+    wid_list_bot = [
                 #widget.DebugInfo(foreground=colors[7], background=colors[0], fontshadow=colors[2]),
                 #### Spacer ####
                 widget.Spacer(
@@ -783,14 +507,14 @@ def init_widgets_bott():
                 #    background=colors[0],
                 #    foreground=colors[7]
                 #    ),
-                widget.Battery(
-                    battery="BAT1",
-                    format='{char} {percent:2.0%} {hour:d}:{min:02d}',
-                    show_short_text=True,
-                    update_interval=10,
-                    background=colors[0],
-                    foreground=colors[7]
-                    ),
+                #widget.Battery(
+                #    battery=0,
+                #    format='{char} {percent:2.0%} {hour:d}:{min:02d}',
+                #    show_short_text=True,
+                #    update_interval=60,
+                #    background=colors[0],
+                #    foreground=colors[7]
+                #    ),
                 #### Systray ####
                 widget.Systray(
                     icon_size=18,
@@ -798,69 +522,23 @@ def init_widgets_bott():
                     foreground=colors[7]
                     ),
                     ]
-    return widgets_bott
+    return wid_list_bot
+
+def in_wid_list_sec():
+    wid_list_sec = [
+                widget.TextBox(
+                    font='Font Awesome 5 Free',
+                    fontsize=15,
+                    foreground=colors[7],
+                    text="",mouse_callbacks={'Button1': lambda: qtile.cmd_spawn(term)},
+                    fontshadow=colors[3]
+                    ),
+                ]
+    return wid_list_sec
 
 #### End Widgets ####
 
-##### Mouse/Keyboard #####
-
-def init_mouse():
-    return [Drag([mod], "Button1", lazy.window.set_position_floating(),      # Move floating windows
-                 start=lazy.window.get_position()),
-            Drag([mod], "Button2", lazy.window.set_size_floating(),          # Resize floating windows
-                 start=lazy.window.get_size()),
-            Click([mod, "shift"], "Button1", lazy.window.bring_to_front())]  # Bring floating window to front
-
-keys = init_keys()
-mouse = init_mouse()
-
-floating_layout = layout.Floating(float_rules=[
-    # Run the utility of `xprop` to see the wm class and name of an X client.
-    # default_float_rules include: utility, notification, toolbar, splash, dialog,
-    # file_progress, confirm, download and error.
-    *layout.Floating.default_float_rules,
-    Match(title='Confirmation'),  # tastyworks exit box
-    Match(title='Qalculate!'),  # qalculate-gtk
-    Match(wm_class='pavucontrol'),  # volume control
-    Match(wm_class='pinentry-gtk-2'),  # GPG key password entry
-    Match(wm_class='lxappearance'),
-    Match(wm_class='confirmreset'),
-    Match(wm_class='makebranch'),
-    Match(wm_class='maketag'),
-    Match(wm_class='branchdialog'),
-    Match(wm_class='pinentry'),
-    Match(wm_class='ssh-askpass'),
-    Match(wm_class='Obconf')
-])
-
-##### End Mouse/Keyboard #####
-
-##### Screens #####
-
-def init_widgets_screen_top():
-    widgets_screen_top = init_widgets_top()
-    return widgets_screen_top
-def init_widgets_screen_bot():
-    widgets_screen_bot = init_widgets_bott()  
-    return widgets_screen_bot
-
-def init_screens():
-    return [
-        Screen(top=bar.Bar(widgets=init_widgets_screen_top(),  size=20, background=colors[0]),
-        bottom=bar.Bar(widgets=init_widgets_screen_bot(), size=20, background=colors[0]))
-        ]
-
-#### End Screens ####
-
-colors = init_colors()
-layout_theme = init_layout_theme()
-layouts = init_layouts()
 widget_defaults = init_widgets_defaults()
-widgets_top = init_widgets_top()
-widgets_bott = init_widgets_bott()
-widgets_screen_top = init_widgets_screen_top()
-widgets_screen_bot = init_widgets_screen_bot()
-screens = init_screens()
-
-#wmname = "LG3D"
-wmname = "qtile"
+wid_list_top = init_wid_list_top()
+wid_list_bot = in_wid_list_bot()
+wid_list_sec = in_wid_list_sec()
